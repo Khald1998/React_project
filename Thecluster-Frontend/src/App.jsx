20962593b88928fdf65f005b4626e8fe0129327f
Khald1998 <108824412+Khald1998@users.noreply.github.com>
2023-04-15 17:55:01 +0300

    -

---
+++ Thecluster-Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Test1</h1>
+      <div>
+        <p>
+          Test2
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
