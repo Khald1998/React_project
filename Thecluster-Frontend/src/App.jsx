30a27438b75cf0806a1ac1042e6524b3aa67a0fc
Khald1998 <108824412+Khald1998@users.noreply.github.com>
2023-04-16 00:16:19 +0300

    -

--- Thecluster-Frontend/src/App.jsx
+++ Thecluster-Frontend/src/App.jsx
@@ -1,18 +1,43 @@
-import Content from './Content'
-import Header from './Header'
-import Footer from './Footer'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import Content from './Content';
+import Header from './Header';
+import Footer from './Footer';
+import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [user, setUser] = useState('');
+  const sub = () =>{
+    axios.get('http://localhost:8080/api')
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
+  
+  };
+  // useEffect(() => {
+  //   fetch('http://localhost:8080/api')
+  //     .then(response => response.json())
+  //     .then(data => setUser(data.user))
+  //     .catch(error => console.log(error));
+  // }, []);
+
 
   return (
-  <>
-    <Header/>
-    <Content/>
-    <Footer/>
-  </>
-
-  )
+    <>
+      <Header/>
+      <Content />
+      <button onClick={sub} className="w-25 mx-auto btn btn-info">sub</button>
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
