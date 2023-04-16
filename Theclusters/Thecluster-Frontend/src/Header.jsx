c467e666d808283da9947d070c4a4862e54b9584
Khald1998 <108824412+Khald1998@users.noreply.github.com>
2023-04-16 23:08:18 +0300

    -

--- Theclusters/Thecluster-Frontend/src/Header.jsx
+++ Theclusters/Thecluster-Frontend/src/Header.jsx
@@ -40,48 +40,53 @@ function Header() {
       </div>
 
       <div className=" d-block d-sm-none ">
-
-      <nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-      <li className="nav-item">
-                <a className="nav-link " href="/">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link " href="/Compose">
-                  Compose A Request
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link " href="/Request">
-                  All The Request
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link " href="/AboutUs">
-                  About Us
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link " href="/ContactUs">
-                  Contact Me
-                </a>
-              </li>
-
-      </ul>
-    </div>
-  </div>
-</nav>
-
-
-
+        <nav className="navbar navbar-expand-lg bg-light">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              The cluster of the clusters
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavDropdown"
+              aria-controls="navbarNavDropdown"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link " href="/">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link " href="/Compose">
+                    Compose A Request
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link " href="/Request">
+                    All The Request
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link " href="/AboutUs">
+                    About Us
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link " href="/ContactUs">
+                    Contact Me
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </div>
     </>
   );
