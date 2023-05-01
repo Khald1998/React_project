04982b498d361679f60c19def4afc35623522d59
Khald1998 <108824412+Khald1998@users.noreply.github.com>
2023-05-01 13:22:01 +0300

    -

--- MyWebsite/src/Header/Header.jsx
+++ MyWebsite/src/Header/Header.jsx
@@ -1,14 +1,29 @@
 import HeaderCSS from "../Style/Header.module.css"
 import Link from "./Link.jsx";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [isTop, setIsTop] = useState(true);
+
+  useEffect(() => {
+    document.addEventListener("scroll", () => {
+      const scrollTop = window.pageYOffset;
+      if (scrollTop === 0) {
+        setIsTop(true);
+      } else {
+        setIsTop(false);
+      }
+    });
+  }, []);
+
   return (
     <>
-      <nav className={ `${HeaderCSS.nav_blur} ${HeaderCSS.nav_bg} navbar navbar-expand fixed-top`}>
+      <nav className={ `${HeaderCSS.nav_blur} ${HeaderCSS.nav_bg} ${isTop ? HeaderCSS.nav_bg_color : ""} navbar navbar-expand fixed-top`}>
         <div className="container-fluid">
           <a className={HeaderCSS.text_nav + " navbar-brand"} href="#">
             Brand
           </a>
-          <ul className="navbar-nav me-auto">
+          <ul className="navbar-nav ms-auto">
           <Link link="link1"/>
           <Link link="link2"/>
           <Link link="link3"/>
