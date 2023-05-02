55e64ffb809420f7edcd4d5205c9ee9d246c0b11
Khald1998 <108824412+Khald1998@users.noreply.github.com>
2023-05-02 14:42:36 +0300

    -

--- Theclusters/Thecluster-Frontend/vite.config.js
+++ Theclusters/Thecluster-Frontend/vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
-    port: 3000
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 3000, // you can replace this port with any port
   }
 })
