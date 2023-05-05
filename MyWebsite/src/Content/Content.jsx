49c5fd04fde486eaa698a498da2b6834c1cd062a
Khald1998 <108824412+Khald1998@users.noreply.github.com>
2023-05-05 14:19:55 +0300

    -

--- MyWebsite/src/Content/Content.jsx
+++ MyWebsite/src/Content/Content.jsx
@@ -2,12 +2,9 @@ import ContentCSS from "../Style/Content.module.css";
 function Content() {
   return (
     <>
-      <section className={`${ContentCSS.sec} text-center pt-5`}>
-        {/* <h1>xxx</h1><h1>xxx</h1><h1>xxx</h1><h1>xxx</h1>
+      <section className={`${ContentCSS.sec} text-center py-5`}>
         <h1>xxx</h1><h1>xxx</h1><h1>xxx</h1><h1>xxx</h1>
         <h1>xxx</h1><h1>xxx</h1><h1>xxx</h1><h1>xxx</h1>
-        <h1>xxx</h1><h1>xxx</h1><h1>xxx</h1><h1>xxx</h1> */}
-
       </section>
     </>
   );
--- MyWebsite/src/More/More.jsx
+++ MyWebsite/src/More/More.jsx
@@ -7,12 +7,12 @@ function More() {
 		<div className={`${MoreCSS.backgroundBR} `}></div>
         <section className={`${MoreCSS.sec} text-center`}>
           <div>
-            {/* <h1>Welcome to Geeks for geeks</h1>
+            <h1>Welcome to Geeks for geeks</h1>
             <h3>Blurred Background Image</h3>
             <p>Example of css program to blur background image</p>
 			<h1>xxx</h1><h1>xxx</h1><h1>xxx</h1><h1>xxx</h1>
 			<h1>xxx</h1><h1>xxx</h1><h1>xxx</h1><h1>xxx</h1>
-			<h1>xxx</h1><h1>xxx</h1><h1>xxx</h1><h1>xxx</h1> */}
+			<h1>xxx</h1><h1>xxx</h1><h1>xxx</h1><h1>xxx</h1>
 
           </div>
         </section>
