59c4ff37588f62eaf25b0d4f6c0a38021ee9c118
Khald1998 <108824412+Khald1998@users.noreply.github.com>
2023-05-06 09:19:58 +0300

    -

---
+++ MyWebsite/src/Introduction/Introduction.jsx
@@ -0,0 +1,14 @@
+import IntroductionCSS from "../Style/Introduction.module.css";
+function Introduction() {
+  return (
+    <>
+      <section className={`${IntroductionCSS.sec} text-center py-5`}>
+        <h1>xxx</h1><h1>xxx</h1><h1>xxx</h1><h1>xxx</h1>
+        <h1>xxx</h1><h1>xxx</h1><h1>xxx</h1><h1>xxx</h1>
+      </section>
+    </>
+  );
+}
+
+export default Introduction;
+
