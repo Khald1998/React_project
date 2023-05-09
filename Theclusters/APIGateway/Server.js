3bcdc59be9065861774efe3a09c82c02530db36e
Khald1998 <108824412+Khald1998@users.noreply.github.com>
2023-05-09 09:35:08 +0300

    -

--- Theclusters/APIGateway/Server.js
+++ Theclusters/APIGateway/Server.js
@@ -5,10 +5,11 @@ const app = express();
 app.use(express.json());
 
 const port = process.env.PORT ||8080;
+const test = process.env.TEST ||"test";
 const ADD = process.env.ADD ||"http://localhost:7001/Add";
 const FIND = process.env.FIND ||"http://localhost:7003/Find";
 const VIEW = process.env.VIEW ||"http://localhost:7002/View";
-
+console.log(test);
 app.post('/Add', (req, res) => {
   const { name, phone, idea, cluster } = req.body;
   request.post(ADD, {
@@ -32,7 +33,17 @@ app.get('/View', (req, res) => {
     });
 });
   
-
+app.get('/Find/:id', (req, res) => {
+    const { id } = req.params;
+    request.get(`${FIND}/${id}`, (error, response, body) => {
+      if (error) {
+        res.status(500).json({ error });
+      } else {
+        res.json(JSON.parse(body));
+      }
+    });
+  });
+  
 
 
 app.listen(port, () => {
