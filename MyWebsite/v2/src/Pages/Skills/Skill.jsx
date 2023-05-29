9a54106fc506c00df7d1b9a3e3b327ab8da44b63
Khald1998 <108824412+Khald1998@users.noreply.github.com>
2023-05-29 13:20:45 +0300

    -

--- MyWebsite/v2/src/Pages/Skills/Skill.jsx
+++ MyWebsite/v2/src/Pages/Skills/Skill.jsx
@@ -1,18 +1,15 @@
-import { FaReact } from "react-icons/fa";
 import { IconContext } from "react-icons";
 
-function Skill() {
+function Skill({ icon, text }) {
   return (
     <>
-    <IconContext.Provider value={{ size:"5em" }}>
-      <div className="w-28 h-32 flex flex-col justify-center items-center mx-auto">
-        <div className="">
-        <FaReact />
+      <IconContext.Provider value={{ size: "5em" }}>
+        <div className="w-28 h-32 flex flex-col justify-center items-center mx-auto">
+          <div className="">{icon}</div>
+          <div className="">
+            <p>{text}</p>
+          </div>
         </div>
-        <div className="">
-            <p>FaReact</p>
-        </div>
-      </div>
       </IconContext.Provider>
     </>
   );
--- MyWebsite/v2/src/Pages/Skills/main.jsx
+++ MyWebsite/v2/src/Pages/Skills/main.jsx
@@ -3,7 +3,14 @@ import SkillsArea from "./SkillsArea";
 function main() {
   return (
     <>
-      <section className="bg-second h-screen w-full">
+      <section className="bg-second w-full
+      xxs:pb-28 
+      xs:pb-28 
+      sm:pb-28 
+      md:pb-28 
+      lg:pb-28
+      xl:h-screen 
+      ">
         <SkillsHeadline/>
         <SkillsArea/>
       </section>
