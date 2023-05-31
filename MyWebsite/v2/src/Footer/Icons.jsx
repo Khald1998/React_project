a3452153a1d4c3447fd74f3e51d454f4c2e565e4
Khald1998 <108824412+Khald1998@users.noreply.github.com>
2023-05-31 13:45:32 +0300

    -

--- MyWebsite/v2/src/Footer/Icons.jsx
+++ MyWebsite/v2/src/Footer/Icons.jsx
@@ -2,7 +2,9 @@ import Icon from "./Icon.jsx";
 function Icons() {
   return (
     <>
-      <section className="my-10 text-center flex flex-row justify-center items-center ">
+      <section className="my-10 text-center flex flex-row justify-center items-center 
+      
+      ">
         <Icon name="Github" href = "https://github.com/Khald1998" />
         <Icon name="Whatsapp" href = "https://api.whatsapp.com/send?phone=966580599359" />
         <Icon name="Linkedin" href = "http://www.linkedin.com/in/khaled-al-zahrani" />
--- MyWebsite/v2/src/Footer/Links.jsx
+++ MyWebsite/v2/src/Footer/Links.jsx
@@ -3,7 +3,9 @@ function Links() {
   return (
     <>
       <section className="pt-5 mb-10 w-4/6 mx-auto">
-        <div className="pt-5 flex text-center flex-row justify-center items-center">
+        <div className="pt-5 flex text-center flex-row justify-center items-center
+        max-md:flex-col
+        ">
           <Link name="ABOUT" />
           <Link name="WORK" />
           <Link name="CONTACT" />
