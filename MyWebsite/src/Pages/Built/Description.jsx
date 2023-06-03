aa069269462ca853b977d46658d0cdaf8b74d7ba
Khald1998 <108824412+Khald1998@users.noreply.github.com>
2023-06-03 13:44:17 +0300

    -

---
+++ MyWebsite/src/Pages/Built/Description.jsx
@@ -0,0 +1,12 @@
+function Description() {
+    return (
+      <>
+              <div className="h-3/4 w-3/4 absolute right-[5%] top-0 bottom-0 my-auto flex justify-end items-center" >
+                <div className="bg-footer w-[60%] h-[28%] rounded-md">  </div>
+              </div>
+      </>
+    );
+  }
+  
+  export default Description;
+  
