1f1ce721b2c5da6c23bcc392932616643f7d95e3
Khald1998 <alzahrani.khaled.98@gmail.com>
2023-07-04 03:24:33 -0700

    Update Warning.jsx

--- Baqalti/Baqalti/frontend/src/Pages/RegisterPage/Warning.jsx
+++ Baqalti/Baqalti/frontend/src/Pages/RegisterPage/Warning.jsx
@@ -2,7 +2,7 @@ function Warning(props) {
   return (
     <>
         <div
-          className="bg-red-100 mx-14 p-4 text-red-700 rounded-3xl"
+          className="bg-red-100 max-sm:p-4 max-sm:mx-4 xl:mx-14 xl:p-4 text-red-700 rounded-3xl"
           role="alert"
         >
           <p className="font-bold"> {props.amount} Error{props.amount>1?'s':''}</p>
